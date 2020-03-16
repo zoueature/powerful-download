f8a1971ab8bc42d3e707cb9df4e55dc7f66b8678
zoueature <zoueature@gmail.com>
2020-03-16 22:56:17 +0800

    update something

--- modules/download/torrent.go
+++ modules/download/torrent.go
@@ -15,9 +15,8 @@
 package download
 
 import (
-	"io/ioutil"
-	"os"
 	"strconv"
+	"errors"
 )
 
 type torrentData struct {
@@ -34,7 +33,7 @@ const (
 	bDecodeEnd    = 'e'
 )
 
-var test = "d13:announce-listll30:http://t.t789.co:2710/announceel30:http://t.t789.me:2710/announceel31:http://t.t789.vip:2710/announceee4:infod6:lengthi1263502296e4:name71:[电影天堂www.dytt89.com]局内人2：头号通缉BD中英双字.mp412:piece lengthi524288e6:pieces48200:"
+var test = "d13:announce-listll30:http://t.t789.co:2710/announceel30:http://t.t789.me:2710/announceel31:http://t.t789.vip:2710/announceee4:infod6:lengthi1263502296e4:name71:[电影天堂www.dytt89.com]局内人2：头号通缉BD中英双字.mp412:piece lengthi524288e6:pieces1:de"
 
 type TorrentDownloader struct {
 	Path string
@@ -59,22 +58,26 @@ func (tc *TorrentDownloader) cancel(task *downTask) error {
 }
 
 func (tc *TorrentDownloader) parseURLInfo(url string) (*downloadInfo, error) {
-	fileReader, err := os.Open(url)
-	if err != nil {
-		return nil, err
-	}
-	torrentContent, err := ioutil.ReadAll(fileReader)
-	if err != nil {
-		return nil, err
-	}
-	//torrentContent := []byte(url)
+	//fileReader, err := os.Open(url)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//torrentContent, err := ioutil.ReadAll(fileReader)
+	//if err != nil {
+	//	return nil, err
+	//}
+	return &downloadInfo{}, nil
+}
+
+func bDecode(info string) (interface{}, error) {
+	torrentContent := []byte("d13:announce-listll30:http://t.t789.co:2710/announceel30:http://t.t789.me:2710/announceel31:http://t.t789.vip:2710/announceee4:infod6:lengthi1263502296e4:name71:[电影天堂www.dytt89.com]局内人2：头号通缉BD中英双字.mp412:piece lengthi524288e6:pieces1:de")
 	//bdecode
 	var typeStack []byte
 	var matchContainer [][]byte
 	var strMatcher []byte  //存储匹配的字符串长度
 	var numMatcher []byte  //存储匹配的数值
 	var startNumMatch bool //标识是否开启数值匹配
-	info := new(torrentData)
+	var tmp []interface{}
 	for index, b := range torrentContent {
 		var nowType byte
 		if len(typeStack) > 0 {
@@ -100,15 +103,44 @@ func (tc *TorrentDownloader) parseURLInfo(url string) (*downloadInfo, error) {
 			str := torrentContent[index+1:index+strLen]
 			matchContainer = append(matchContainer, str)
 			strMatcher = []byte{}
+			typeStack = append(typeStack, bDecodeString)
 		} else if b == bDecodeEnd {
 			if startNumMatch {
 				//数值匹配
 				matchContainer = append(matchContainer, numMatcher)
 				startNumMatch = false
 				numMatcher = []byte{}
+				typeStack = append(typeStack, bDecodeInt)
 				continue
+			}
+			if nowType == bDecodeInt || nowType == bDecodeString {
+				tmp = append(tmp, matchContainer[len(matchContainer)-1])
+				typeStack = append(typeStack[len(typeStack)-1:])
+				matchContainer = append(matchContainer[len(matchContainer)-1:])
+			} else if nowType == bDecodeList {
+				tmp = []interface{}{tmp}
+			} else if nowType == bDecodeHash {
+				var mapKey []interface{}
+				var mapValue []interface{}
+				for index, v := range tmp {
+					if index % 2 == 0 {
+						mapKey = append(mapKey, v)
+					} else {
+						mapValue = append(mapValue, v)
+					}
+				}
+				dic := make(map[string]interface{})
+				for index, key := range mapKey {
+					sKey, ok := key.(string)
+					if !ok {
+						return nil, errors.New("error")
+					}
+					dic[sKey] = mapValue[index]
+				}
+				tmp = []interface{}{dic}
 			}
 		}
 	}
-	return nil, nil
+	return tmp, nil
 }
+
